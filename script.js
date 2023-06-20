df0975c0dbd7fdded351bd73fd461ec88c928658
rileyham <rileyham21@gmail.com>
2023-06-19 23:22:37 -0400

    add functionality to calculator

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const digits = document.getElementsByClassName("digit");
 const screen = document.getElementById("screen");
 const clear = document.getElementById("clear");
+const equals = document.getElementById("equals");
+const operators = document.getElementsByClassName("operator");
+
 let num1;
 let num2;
 let operator;
 let displayValue = 0;
+let numToggle = false;
 
 for (let i = 0; i < digits.length; i++) {
     digits[i].addEventListener("click", function() {
@@ -12,10 +16,27 @@ for (let i = 0; i < digits.length; i++) {
         screen.textContent = displayValue;
     });
 }
+
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener("click", function() {
+        num1 = displayValue;
+        displayValue = 0;
+        operator = operators[i].textContent;
+    });
+}
+
 clear.addEventListener("click", function() {
     displayValue = 0;
     screen.textContent = displayValue;
 });
+
+equals.addEventListener("click", function() {
+    num2 = displayValue;
+    displayValue = operate(num1, num2, operator);
+    screen.textContent = displayValue;
+});
+
+
 
 
 function add(a,b) {
@@ -34,13 +55,13 @@ function divide(a,b) {
 }
 
 function operate(num1, num2, operator) {
-    if (operator=="add") {
+    if (operator=="+") {
         return add(num1,num2);
     }
-    else if (operator=="subtract") {
+    else if (operator=="-") {
         return subtract(num1,num2);
     }
-    else if (operator=="multiply") {
+    else if (operator=="X") {
         return multiply(num1,num2);
     }
     else {
